344748ba861de0d8be447836dbc111fa12cc8dbe
Ashish250 <ashstipulations@gmail.com>
2016-11-19 16:00:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -132,16 +132,18 @@ app.get('/articles/:articleName', function (req, res){
         else
              {
                     if(result.rows.length === 0)
+                    {
                     res.status(404).send('Article not found');
-             }
+                    }
              else
-                {
+                    {
                         var articleData = result.rows[0];
-                        res.send(createTemplate(articleData);
-                }
+                        res.send(createTemplate(articleData));
+                    }
     }
                     });
-    });
+});
+    
 
 
 app.get('/ui/style.css', function (req, res) {
